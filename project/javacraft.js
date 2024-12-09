a6be27afbf02972614506e7701762baddb6b0aa3
edmundroxas <erroxas@aggies.ncat.edu>
2024-12-08 19:37:12 -0600

    Update javacraft.js

--- project/javacraft.js
+++ project/javacraft.js
@@ -6,3 +6,42 @@ $slideshow.on("click", function() {
    else
       $slideshow.cycle("pause");
 });
+$(document).ready(function() {
+            // Add event listeners using jQuery
+            $('#homeLink').click(function(event) {
+                alert('Home page link clicked!');
+            });
+
+            $('#aboutLink').hover(function() {
+                alert('Hovered over About Us link!');
+            });
+
+            // Click event on image
+            $('#heroImage').click(function() {
+                alert('Hero image clicked!');
+            });
+
+            // Social media links: adding hover effects
+            $('#facebookLink').hover(function() {
+                $(this).css('color', '#3b5998'); // Facebook blue
+            }, function() {
+                $(this).css('color', ''); // Reset color when hover ends
+            });
+
+            $('#twitterLink').hover(function() {
+                $(this).css('color', '#1DA1F2'); // Twitter blue
+            }, function() {
+                $(this).css('color', ''); // Reset color when hover ends
+            });
+
+            $('#instagramLink').hover(function() {
+                $(this).css('color', '#C13584'); // Instagram purple
+            }, function() {
+                $(this).css('color', ''); // Reset color when hover ends
+            });
+        });
+
+        // Plain JavaScript click event handler for logo
+        document.getElementById('logo').addEventListener('click', function() {
+            alert('Logo clicked!');
+        });
