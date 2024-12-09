96f23e1f2ea0fd07b38d28d6878ec6cdfc6562fe
edmundroxas <erroxas@aggies.ncat.edu>
2024-12-08 22:09:05 -0600

    Update javacraft.js

--- project/javacraft.js
+++ project/javacraft.js
@@ -1,50 +1,52 @@
-let $slideshow = $(".cycle-slideshow");
-
-$slideshow.on("click", function() {
-   if ($slideshow.is(".cycle-paused"))
-      $slideshow.cycle("resume");                
-   else
-      $slideshow.cycle("pause");
-});
-
-$("#datepicker").datepicker();
-
 $(document).ready(function() {
-            // Add event listeners using jQuery
-            $('#homeLink').click(function(event) {
-                alert('Home page link clicked!');
-            });
+    // Slideshow toggle for Cycle2
+    let $slideshow = $(".cycle-slideshow");
+    
+    $slideshow.on("click", function() {
+        if ($slideshow.is(".cycle-paused"))
+            $slideshow.cycle("resume");                
+        else
+            $slideshow.cycle("pause");
+    });
+    
+    // Datepicker initialization
+    $("#datepicker").datepicker();
+    
+    // Add event listeners using jQuery
+    $('#homeLink').click(function(event) {
+        alert('Home page link clicked!');
+    });
 
-            $('#aboutLink').hover(function() {
-                alert('Hovered over About Us link!');
-            });
+    $('#aboutLink').hover(function() {
+        alert('Hovered over About Us link!');
+    });
 
-            // Click event on image
-            $('#heroImage').click(function() {
-                alert('Hero image clicked!');
-            });
+    // Click event on image
+    $('#heroImage').click(function() {
+        alert('Hero image clicked!');
+    });
 
-            // Social media links: adding hover effects
-            $('#facebookLink').hover(function() {
-                $(this).css('color', '#3b5998'); // Facebook blue
-            }, function() {
-                $(this).css('color', ''); // Reset color when hover ends
-            });
+    // Social media links: adding hover effects
+    $('#facebookLink').hover(function() {
+        $(this).css('color', '#3b5998'); // Facebook blue
+    }, function() {
+        $(this).css('color', ''); // Reset color when hover ends
+    });
 
-            $('#twitterLink').hover(function() {
-                $(this).css('color', '#1DA1F2'); // Twitter blue
-            }, function() {
-                $(this).css('color', ''); // Reset color when hover ends
-            });
+    $('#twitterLink').hover(function() {
+        $(this).css('color', '#1DA1F2'); // Twitter blue
+    }, function() {
+        $(this).css('color', ''); // Reset color when hover ends
+    });
 
-            $('#instagramLink').hover(function() {
-                $(this).css('color', '#C13584'); // Instagram purple
-            }, function() {
-                $(this).css('color', ''); // Reset color when hover ends
-            });
-        });
+    $('#instagramLink').hover(function() {
+        $(this).css('color', '#C13584'); // Instagram purple
+    }, function() {
+        $(this).css('color', ''); // Reset color when hover ends
+    });
 
-        // Plain JavaScript click event handler for logo
-        document.getElementById('logo').addEventListener('click', function() {
-            alert('Logo clicked!');
-        });
+    // Plain JavaScript click event handler for logo
+    $('#logo').click(function() {
+        alert('Logo clicked!');
+    });
+});
